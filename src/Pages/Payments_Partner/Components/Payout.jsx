98ec4f36c1a0ca0494adcb43e59d9f046ab9f1c7
Adeel <adeel.abid@ziscomm.me>
2026-01-21 14:32:52 +0500

    updated

---
+++ src/Pages/Payments_Partner/Components/Payout.jsx
@@ -0,0 +1,9 @@
+const Payout = () => {
+  return (
+    <div>
+      <h1>payout</h1>
+    </div>
+  );
+};
+
+export default Payout;
--- src/Pages/Payments_Partner/Payments_Partner.jsx
+++ src/Pages/Payments_Partner/Payments_Partner.jsx
@@ -1,7 +1,42 @@
+import { useState, lazy, Suspense } from "react";
+import Segment from "../../Components/Segment/Segment";
+const Payout = lazy(() => import("./Components/Payout"));
+const Overview = lazy(() => import("./Components/Overview"));
+
 const Payments_Partner = () => {
+  const [activeComponent, setActiveComponent] = useState(1);
+
+  const tabs = [
+    {
+      id: 1,
+      label: "Overview",
+      component: <Overview />,
+    },
+    {
+      id: 2,
+      label: "Payout",
+      component: <Payout />,
+    },
+  ];
+
   return (
-    <div>
-      <h1>Partner payments</h1>
+    <div className="flex flex-col w-full items-center h-full overflow-auto py-11 relative z-10 overflow-x-hidden">
+      <div className="flex flex-col gap-10 w-[72%]">
+        <div className="flex flex-col gap-1">
+          <h1 className="text-2xl font-bold! text-[#262626] leading-11">
+            Payments and Payouts
+          </h1>
+          <p className="text-[16px] font-normal text-[#737373] leading-6 mb-11">
+            Track your earnings, manage payout methods, and view transaction
+            history in one place.
+          </p>
+        </div>
+        <Segment
+          tabs={tabs}
+          defaultActiveTab={activeComponent}
+          onTabChange={setActiveComponent}
+        />
+      </div>
     </div>
   );
 };
