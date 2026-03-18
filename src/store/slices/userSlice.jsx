5e92bf66bf94fe2d8c2a6f00f0995a9ca7fc9314
Adeel <adeel.abid@ziscomm.me>
2026-03-18 09:54:44 +0500

    updated

--- src/store/slices/userSlice.jsx
+++ src/store/slices/userSlice.jsx
@@ -4,7 +4,7 @@ export const userSlice = createSlice({
   name: "user",
   initialState: {
     user: {
-      role: "isSuperAdmin",
+      role: "isCustomer",
       // isCustomer,
       // isPartner,
       // isAdmin:,
