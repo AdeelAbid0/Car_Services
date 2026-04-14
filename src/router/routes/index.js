b9eea01305c83662b9f3d044faf012fcbbbf36a1
Adeel <adeel.abid@ziscomm.me>
2026-04-14 15:24:51 +0500

    changes file extension of index.js to jsx in routes

--- src/router/routes/index.js
+++
@@ -1,7 +0,0 @@
-// exporting all routes from same file
-
-export { AUTH_ROUTES } from "./auth.routes";
-export { CUSTOMER_ROUTES } from "./customer.routes";
-export { PARTNER_ROUTES } from "./partner.routes";
-export { SUPERADMIN_ROUTES } from "./superadmin.routes";
-export { PUBLIC_ROUTES } from "./public.routes";
