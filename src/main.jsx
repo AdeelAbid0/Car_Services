22e50146a44d95d85b178f3e3016e7c0f1470894
Adeel <adeel.abid@ziscomm.me>
2026-04-14 11:41:55 +0500

    resolved deployment bug in layout

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.jsx";
+import App from "./App";
 import "antd/dist/reset.css";
 import { Provider } from "react-redux";
-import store from "./store/store.jsx";
-import ErrorBoundary from "./shared/components/common/ErrorBoundary.jsx";
+import store from "./store/store";
+import ErrorBoundary from "./shared/components/common/ErrorBoundary";
 
 createRoot(document.getElementById("root")).render(
   <ErrorBoundary>
